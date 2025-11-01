5ea44b347e68fad92f8e838319ce90072cd75125
Ben House <25386958+Swissman1@users.noreply.github.com>
2025-11-01 06:35:30 -0400

    prototype!

--- snapshotMaps/2025/layers/MissingRoads.js
+++ snapshotMaps/2025/layers/MissingRoads.js
@@ -1,1 +1,3 @@
+var cullFactor = 1;
+var lastYear = 2005;
 var json_MissingRoads = {"type":"FeatureCollection","name":"MissingRoads","crs":{"type":"name","properties":{"name":"urn:ogc:def:crs:OGC:1.3:CRS84"}},"features":[]}
