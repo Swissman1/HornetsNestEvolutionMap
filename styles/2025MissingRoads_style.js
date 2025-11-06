f85ca738c9dd7ce0f5dcc0ac65a28b172617f1f0
Ben House <25386958+Swissman1@users.noreply.github.com>
2025-11-06 16:22:50 -0500

    fixed map

--- styles/2025MissingRoads_style.js
+++ styles/2025MissingRoads_style.js
@@ -1,7 +1,7 @@
 var size = 0;
 var placement = 'point';
 
-var style_2025MissingRoads = function(feature, resolution){
+var style_MissingRoads = function(feature, resolution){
     var context = {
         feature: feature,
         variables: {}
