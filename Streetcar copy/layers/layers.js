558a0a14ae9dd9b6062480dade9986e971bd9b22
Ben House <25386958+Swissman1@users.noreply.github.com>
2025-11-14 09:38:36 -0500

    more attempintg

---
+++ Streetcar copy/layers/layers.js
@@ -0,0 +1,68 @@
+var wms_layers = [];
+
+
+        var lyr_OSMStandard = new ol.layer.Tile({
+            'title': 'OSM Standard',
+            'opacity': 1.000000,
+            
+            
+            source: new ol.source.XYZ({
+            attributions: ' &nbsp &middot; <a href="https://www.openstreetmap.org/copyright">© OpenStreetMap contributors, CC-BY-SA</a>',
+                url: 'http://tile.openstreetmap.org/{z}/{x}/{y}.png'
+            })
+        });
+var format_streetcarstreets = new ol.format.GeoJSON();
+var features_streetcarstreets = format_streetcarstreets.readFeatures(json_streetcarstreets, 
+            {dataProjection: 'EPSG:4326', featureProjection: 'EPSG:3857'});
+var jsonSource_streetcarstreets = new ol.source.Vector({
+    attributions: ' ',
+});
+jsonSource_streetcarstreets.addFeatures(features_streetcarstreets);
+var lyr_streetcarstreets = new ol.layer.Vector({
+                declutter: false,
+                source:jsonSource_streetcarstreets, 
+                style: style_streetcarstreets,
+                popuplayertitle: '1935 streets',
+                interactive: true,
+                title: '<img src="styles/legend/streetcarstreets.png" />1935 streets'
+            });
+var format_streetcardisp = new ol.format.GeoJSON();
+var features_streetcardisp = format_streetcardisp.readFeatures(json_streetcardisp, 
+            {dataProjection: 'EPSG:4326', featureProjection: 'EPSG:3857'});
+var jsonSource_streetcardisp = new ol.source.Vector({
+    attributions: ' ',
+});
+jsonSource_streetcardisp.addFeatures(features_streetcardisp);
+var lyr_streetcardisp = new ol.layer.Vector({
+                declutter: false,
+                source:jsonSource_streetcardisp, 
+                style: style_streetcardisp,
+                popuplayertitle: 'Streetcar Lines',
+                interactive: true,
+    title: 'Streetcar Lines<br />\
+    <img src="styles/legend/streetcardisp_1.png" /> 4th Ward Line<br />\
+    <img src="styles/legend/streetcardisp_2.png" /> Brevard Line<br />\
+    <img src="styles/legend/streetcardisp_3.png" /> Brooklyn Line<br />\
+    <img src="styles/legend/streetcardisp_4.png" /> Combined<br />\
+    <img src="styles/legend/streetcardisp_5.png" /> Dilworth Line<br />\
+    <img src="styles/legend/streetcardisp_6.png" /> Eastover Line<br />\
+    <img src="styles/legend/streetcardisp_7.png" /> Gold/Biddleville Line<br />\
+    <img src="styles/legend/streetcardisp_8.png" /> Latta Park Line<br />\
+    <img src="styles/legend/streetcardisp_9.png" /> Myers Park Line<br />\
+    <img src="styles/legend/streetcardisp_10.png" /> Pegram Line<br />\
+    <img src="styles/legend/streetcardisp_11.png" /> Plaza line<br />\
+    <img src="styles/legend/streetcardisp_12.png" /> Wilmore Line<br />\
+    <img src="styles/legend/streetcardisp_13.png" /> 1st Ward Line<br />\
+    <img src="styles/legend/streetcardisp_14.png" /> 1st Ward/Brevard Line<br />' });
+
+lyr_OSMStandard.setVisible(true);lyr_streetcarstreets.setVisible(true);lyr_streetcardisp.setVisible(true);
+var layersList = [lyr_OSMStandard,lyr_streetcarstreets,lyr_streetcardisp];
+lyr_streetcarstreets.set('fieldAliases', {'Name': 'Name', });
+lyr_streetcardisp.set('fieldAliases', {'name': 'name', });
+lyr_streetcarstreets.set('fieldImages', {'Name': 'TextEdit', });
+lyr_streetcardisp.set('fieldImages', {'name': 'TextEdit', });
+lyr_streetcarstreets.set('fieldLabels', {'Name': 'inline label - visible with data', });
+lyr_streetcardisp.set('fieldLabels', {'name': 'inline label - always visible', });
+lyr_streetcardisp.on('precompose', function(evt) {
+    evt.context.globalCompositeOperation = 'normal';
+});
---
+++ Streetcar copy/styles/streetcardisp_style.js
@@ -0,0 +1,233 @@
+var size = 0;
+var placement = 'point';
+function categories_streetcardisp(feature, value, size, resolution, labelText,
+                       labelFont, labelFill, bufferColor, bufferWidth,
+                       placement) {
+                var valueStr = (value !== null && value !== undefined) ? value.toString() : 'default';
+                switch(valueStr) {default:
+                    return [ new ol.style.Style({
+        stroke: new ol.style.Stroke({color: 'rgba(213,68,157,1.0)', lineDash: null, lineCap: 'butt', lineJoin: 'miter', width: 4.56}),
+        text: createTextStyle(feature, resolution, labelText, labelFont,
+                              labelFill, placement, bufferColor,
+                              bufferWidth)
+    }),new ol.style.Style({
+        stroke: new ol.style.Stroke({color: 'rgba(213,68,157,1.0)', lineDash: null, lineCap: 'square', lineJoin: 'bevel', width: 3.268}),
+        text: createTextStyle(feature, resolution, labelText, labelFont,
+                              labelFill, placement, bufferColor,
+                              bufferWidth)
+    })];
+                    break;
+case '4th Ward Line':
+                    return [ new ol.style.Style({
+        stroke: new ol.style.Stroke({color: 'rgba(2,22,7,1.0)', lineDash: null, lineCap: 'butt', lineJoin: 'miter', width: 4.56}),
+        text: createTextStyle(feature, resolution, labelText, labelFont,
+                              labelFill, placement, bufferColor,
+                              bufferWidth)
+    }),new ol.style.Style({
+        stroke: new ol.style.Stroke({color: 'rgba(26,237,79,1.0)', lineDash: null, lineCap: 'square', lineJoin: 'bevel', width: 3.8}),
+        text: createTextStyle(feature, resolution, labelText, labelFont,
+                              labelFill, placement, bufferColor,
+                              bufferWidth)
+    })];
+                    break;
+case 'Brevard Line':
+                    return [ new ol.style.Style({
+        stroke: new ol.style.Stroke({color: 'rgba(3,3,3,1.0)', lineDash: null, lineCap: 'butt', lineJoin: 'miter', width: 4.56}),
+        text: createTextStyle(feature, resolution, labelText, labelFont,
+                              labelFill, placement, bufferColor,
+                              bufferWidth)
+    }),new ol.style.Style({
+        stroke: new ol.style.Stroke({color: 'rgba(28,110,23,1.0)', lineDash: null, lineCap: 'square', lineJoin: 'bevel', width: 3.8}),
+        text: createTextStyle(feature, resolution, labelText, labelFont,
+                              labelFill, placement, bufferColor,
+                              bufferWidth)
+    })];
+                    break;
+case 'Brooklyn Line':
+                    return [ new ol.style.Style({
+        stroke: new ol.style.Stroke({color: 'rgba(66,21,31,1.0)', lineDash: null, lineCap: 'butt', lineJoin: 'miter', width: 4.75}),
+        text: createTextStyle(feature, resolution, labelText, labelFont,
+                              labelFill, placement, bufferColor,
+                              bufferWidth)
+    }),new ol.style.Style({
+        stroke: new ol.style.Stroke({color: 'rgba(206,65,96,1.0)', lineDash: null, lineCap: 'square', lineJoin: 'bevel', width: 3.9899999999999998}),
+        text: createTextStyle(feature, resolution, labelText, labelFont,
+                              labelFill, placement, bufferColor,
+                              bufferWidth)
+    })];
+                    break;
+case 'Combined':
+                    return [ new ol.style.Style({
+        stroke: new ol.style.Stroke({color: 'rgba(25,49,65,1.0)', lineDash: null, lineCap: 'butt', lineJoin: 'miter', width: 6.08}),
+        text: createTextStyle(feature, resolution, labelText, labelFont,
+                              labelFill, placement, bufferColor,
+                              bufferWidth)
+    }),new ol.style.Style({
+        stroke: new ol.style.Stroke({color: 'rgba(68,134,175,1.0)', lineDash: null, lineCap: 'square', lineJoin: 'bevel', width: 4.192653999999999}),
+        text: createTextStyle(feature, resolution, labelText, labelFont,
+                              labelFill, placement, bufferColor,
+                              bufferWidth)
+    })];
+                    break;
+case 'Dilworth Line':
+                    return [ new ol.style.Style({
+        stroke: new ol.style.Stroke({color: 'rgba(169,121,25,1.0)', lineDash: null, lineCap: 'butt', lineJoin: 'miter', width: 4.56}),
+        text: createTextStyle(feature, resolution, labelText, labelFont,
+                              labelFill, placement, bufferColor,
+                              bufferWidth)
+    }),new ol.style.Style({
+        stroke: new ol.style.Stroke({color: 'rgba(169,121,25,1.0)', lineDash: null, lineCap: 'square', lineJoin: 'bevel', width: 3.268}),
+        text: createTextStyle(feature, resolution, labelText, labelFont,
+                              labelFill, placement, bufferColor,
+                              bufferWidth)
+    })];
+                    break;
+case 'Eastover Line':
+                    return [ new ol.style.Style({
+        stroke: new ol.style.Stroke({color: 'rgba(146,66,46,1.0)', lineDash: null, lineCap: 'butt', lineJoin: 'miter', width: 4.56}),
+        text: createTextStyle(feature, resolution, labelText, labelFont,
+                              labelFill, placement, bufferColor,
+                              bufferWidth)
+    }),new ol.style.Style({
+        stroke: new ol.style.Stroke({color: 'rgba(146,66,46,1.0)', lineDash: null, lineCap: 'square', lineJoin: 'bevel', width: 3.268}),
+        text: createTextStyle(feature, resolution, labelText, labelFont,
+                              labelFill, placement, bufferColor,
+                              bufferWidth)
+    })];
+                    break;
+case 'Gold/Biddleville Line':
+                    return [ new ol.style.Style({
+        stroke: new ol.style.Stroke({color: 'rgba(42,39,8,1.0)', lineDash: null, lineCap: 'butt', lineJoin: 'miter', width: 4.75}),
+        text: createTextStyle(feature, resolution, labelText, labelFont,
+                              labelFill, placement, bufferColor,
+                              bufferWidth)
+    }),new ol.style.Style({
+        stroke: new ol.style.Stroke({color: 'rgba(213,195,41,1.0)', lineDash: null, lineCap: 'square', lineJoin: 'bevel', width: 3.8}),
+        text: createTextStyle(feature, resolution, labelText, labelFont,
+                              labelFill, placement, bufferColor,
+                              bufferWidth)
+    })];
+                    break;
+case 'Latta Park Line':
+                    return [ new ol.style.Style({
+        stroke: new ol.style.Stroke({color: 'rgba(81,65,43,1.0)', lineDash: null, lineCap: 'butt', lineJoin: 'miter', width: 4.56}),
+        text: createTextStyle(feature, resolution, labelText, labelFont,
+                              labelFill, placement, bufferColor,
+                              bufferWidth)
+    }),new ol.style.Style({
+        stroke: new ol.style.Stroke({color: 'rgba(154,167,110,1.0)', lineDash: null, lineCap: 'square', lineJoin: 'bevel', width: 3.268}),
+        text: createTextStyle(feature, resolution, labelText, labelFont,
+                              labelFill, placement, bufferColor,
+                              bufferWidth)
+    })];
+                    break;
+case 'Myers Park Line':
+                    return [ new ol.style.Style({
+        stroke: new ol.style.Stroke({color: 'rgba(59,105,104,1.0)', lineDash: null, lineCap: 'butt', lineJoin: 'miter', width: 4.56}),
+        text: createTextStyle(feature, resolution, labelText, labelFont,
+                              labelFill, placement, bufferColor,
+                              bufferWidth)
+    }),new ol.style.Style({
+        stroke: new ol.style.Stroke({color: 'rgba(128,229,227,1.0)', lineDash: null, lineCap: 'square', lineJoin: 'bevel', width: 3.268}),
+        text: createTextStyle(feature, resolution, labelText, labelFont,
+                              labelFill, placement, bufferColor,
+                              bufferWidth)
+    })];
+                    break;
+case 'Pegram Line':
+                    return [ new ol.style.Style({
+        stroke: new ol.style.Stroke({color: 'rgba(45,86,34,1.0)', lineDash: null, lineCap: 'butt', lineJoin: 'miter', width: 4.75}),
+        text: createTextStyle(feature, resolution, labelText, labelFont,
+                              labelFill, placement, bufferColor,
+                              bufferWidth)
+    }),new ol.style.Style({
+        stroke: new ol.style.Stroke({color: 'rgba(106,203,81,1.0)', lineDash: null, lineCap: 'square', lineJoin: 'bevel', width: 3.8}),
+        text: createTextStyle(feature, resolution, labelText, labelFont,
+                              labelFill, placement, bufferColor,
+                              bufferWidth)
+    })];
+                    break;
+case 'Plaza Line':
+                    return [ new ol.style.Style({
+        stroke: new ol.style.Stroke({color: 'rgba(32,84,35,1.0)', lineDash: null, lineCap: 'butt', lineJoin: 'miter', width: 4.56}),
+        text: createTextStyle(feature, resolution, labelText, labelFont,
+                              labelFill, placement, bufferColor,
+                              bufferWidth)
+    }),new ol.style.Style({
+        stroke: new ol.style.Stroke({color: 'rgba(50,52,201,1.0)', lineDash: null, lineCap: 'square', lineJoin: 'bevel', width: 3.268}),
+        text: createTextStyle(feature, resolution, labelText, labelFont,
+                              labelFill, placement, bufferColor,
+                              bufferWidth)
+    })];
+                    break;
+case 'Wilmore Line':
+                    return [ new ol.style.Style({
+        stroke: new ol.style.Stroke({color: 'rgba(99,82,27,1.0)', lineDash: null, lineCap: 'butt', lineJoin: 'miter', width: 4.75}),
+        text: createTextStyle(feature, resolution, labelText, labelFont,
+                              labelFill, placement, bufferColor,
+                              bufferWidth)
+    }),new ol.style.Style({
+        stroke: new ol.style.Stroke({color: 'rgba(77,153,22,1.0)', lineDash: null, lineCap: 'square', lineJoin: 'bevel', width: 3.268}),
+        text: createTextStyle(feature, resolution, labelText, labelFont,
+                              labelFill, placement, bufferColor,
+                              bufferWidth)
+    })];
+                    break;
+case '1st Ward Line':
+                    return [ new ol.style.Style({
+        stroke: new ol.style.Stroke({color: 'rgba(14,35,31,1.0)', lineDash: null, lineCap: 'butt', lineJoin: 'miter', width: 4.75}),
+        text: createTextStyle(feature, resolution, labelText, labelFont,
+                              labelFill, placement, bufferColor,
+                              bufferWidth)
+    }),new ol.style.Style({
+        stroke: new ol.style.Stroke({color: 'rgba(201,159,69,1.0)', lineDash: null, lineCap: 'square', lineJoin: 'bevel', width: 3.8}),
+        text: createTextStyle(feature, resolution, labelText, labelFont,
+                              labelFill, placement, bufferColor,
+                              bufferWidth)
+    })];
+                    break;
+case '1st Ward/Brevard Line':
+                    return [ new ol.style.Style({
+        stroke: new ol.style.Stroke({color: 'rgba(35,35,35,1.0)', lineDash: null, lineCap: 'square', lineJoin: 'bevel', width: 7.6}),
+        text: createTextStyle(feature, resolution, labelText, labelFont,
+                              labelFill, placement, bufferColor,
+                              bufferWidth)
+    }),new ol.style.Style({
+        stroke: new ol.style.Stroke({color: 'rgba(201,159,69,1.0)', lineDash: null, lineCap: 'butt', lineJoin: 'miter', width: 7.03}),
+        text: createTextStyle(feature, resolution, labelText, labelFont,
+                              labelFill, placement, bufferColor,
+                              bufferWidth)
+    }),new ol.style.Style({
+        stroke: new ol.style.Stroke({color: 'rgba(50,52,201,1.0)', lineDash: null, lineCap: 'square', lineJoin: 'bevel', width: 3.3587782}),
+        text: createTextStyle(feature, resolution, labelText, labelFont,
+                              labelFill, placement, bufferColor,
+                              bufferWidth)
+    })];
+                    break;}};
+
+var style_streetcardisp = function(feature, resolution){
+    var context = {
+        feature: feature,
+        variables: {}
+    };
+    
+    var labelText = ""; 
+    var value = feature.get("name");
+    var labelFont = "10px, sans-serif";
+    var labelFill = "#000000";
+    var bufferColor = "";
+    var bufferWidth = 0;
+    var textAlign = "left";
+    var offsetX = 0;
+    var offsetY = 0;
+    var placement = 'line';
+    if ("" !== null) {
+        labelText = String("");
+    }
+    
+    var style = categories_streetcardisp(feature, value, size, resolution, labelText,
+                            labelFont, labelFill, bufferColor,
+                            bufferWidth, placement);
+
+    return style;
+};
