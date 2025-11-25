6fba40a19377d10eb0a849020de07e2c444afc48
Ben House <25386958+Swissman1@users.noreply.github.com>
2025-11-25 13:39:52 -0500

    better contrast on annex

--- styles/Annexation_History_style.js
+++ styles/Annexation_History_style.js
@@ -21,42 +21,42 @@ var style_Annexation_History = function(feature, resolution){
     
     if (value >= 0.000000 && value <= 1850.000000) {
             style = [ new ol.style.Style({
-        stroke: new ol.style.Stroke({color: 'rgba(25,20,32,1.0)', lineDash: null, lineCap: 'butt', lineJoin: 'miter', width: 0.988}),fill: new ol.style.Fill({color: 'rgba(9,4,6,1.0)'}),
+        stroke: new ol.style.Stroke({color: 'rgba(5,20,32,1.0)', lineDash: null, lineCap: 'butt', lineJoin: 'miter', width: 1.988}),fill: new ol.style.Fill({color: 'rgba(9,4,6,1.0)'}),
         text: createTextStyle(feature, resolution, labelText, labelFont,
                               labelFill, placement, bufferColor,
                               bufferWidth)
     })]
                     } else if (value >= 1850.000000 && value <= 1908.000000) {
             style = [ new ol.style.Style({
-        stroke: new ol.style.Stroke({color: 'rgba(35,35,35,1.0)', lineDash: null, lineCap: 'butt', lineJoin: 'miter', width: 0.988}),fill: new ol.style.Fill({color: 'rgba(56,42,84,1.0)'}),
+        stroke: new ol.style.Stroke({color: 'rgba(5,15,5,1.0)', lineDash: null, lineCap: 'butt', lineJoin: 'miter', width: 2.188}),fill: new ol.style.Fill({color: 'rgba(56,42,84,1.0)'}),
         text: createTextStyle(feature, resolution, labelText, labelFont,
                               labelFill, placement, bufferColor,
                               bufferWidth)
     })]
                     } else if (value >= 1908.000000 && value <= 1974.000000) {
             style = [ new ol.style.Style({
-        stroke: new ol.style.Stroke({color: 'rgba(35,35,35,1.0)', lineDash: null, lineCap: 'butt', lineJoin: 'miter', width: 0.988}),fill: new ol.style.Fill({color: 'rgba(57,93,156,1.0)'}),
+        stroke: new ol.style.Stroke({color: 'rgba(5,5,35,1.0)', lineDash: null, lineCap: 'butt', lineJoin: 'miter', width: 1.788}),fill: new ol.style.Fill({color: 'rgba(57,93,156,1.0)'}),
         text: createTextStyle(feature, resolution, labelText, labelFont,
                               labelFill, placement, bufferColor,
                               bufferWidth)
     })]
                     } else if (value >= 1974.000000 && value <= 1999.000000) {
             style = [ new ol.style.Style({
-        stroke: new ol.style.Stroke({color: 'rgba(35,35,35,1.0)', lineDash: null, lineCap: 'butt', lineJoin: 'miter', width: 0.988}),fill: new ol.style.Fill({color: 'rgba(52,152,169,1.0)'}),
+        stroke: new ol.style.Stroke({color: 'rgba(35,35,35,1.0)', lineDash: null, lineCap: 'butt', lineJoin: 'miter', width: 1.188}),fill: new ol.style.Fill({color: 'rgba(52,152,169,1.0)'}),
         text: createTextStyle(feature, resolution, labelText, labelFont,
                               labelFill, placement, bufferColor,
                               bufferWidth)
     })]
                     } else if (value >= 1999.000000 && value <= 2020.000000) {
             style = [ new ol.style.Style({
-        stroke: new ol.style.Stroke({color: 'rgba(35,35,35,1.0)', lineDash: null, lineCap: 'butt', lineJoin: 'miter', width: 0.988}),fill: new ol.style.Fill({color: 'rgba(98,207,172,1.0)'}),
+        stroke: new ol.style.Stroke({color: 'rgba(35,35,35,1.0)', lineDash: null, lineCap: 'butt', lineJoin: 'miter', width: 1.088}),fill: new ol.style.Fill({color: 'rgba(98,207,172,1.0)'}),
         text: createTextStyle(feature, resolution, labelText, labelFont,
                               labelFill, placement, bufferColor,
                               bufferWidth)
     })]
                     } else if (value >= 2020.000000 && value <= 197861.000000) {
             style = [ new ol.style.Style({
-        stroke: new ol.style.Stroke({color: 'rgba(35,35,35,1.0)', lineDash: null, lineCap: 'butt', lineJoin: 'miter', width: 0.988}),fill: new ol.style.Fill({color: 'rgba(210,250,251,1.0)'}),
+        stroke: new ol.style.Stroke({color: 'rgba(35,35,35,1.0)', lineDash: null, lineCap: 'butt', lineJoin: 'miter', width: 1.088}),fill: new ol.style.Fill({color: 'rgba(210,250,251,1.0)'}),
         text: createTextStyle(feature, resolution, labelText, labelFont,
                               labelFill, placement, bufferColor,
                               bufferWidth)
